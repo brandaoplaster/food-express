747c6383e86defd2c9103863cfb79a2b4131dc93
brandaoplaster <brandaoplaster@gmail.com>
2024-06-29 18:47:06 -0300

    feat: Adding controller to checkout

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   resources :products, only: [:show]
 
   get "cart" => "carts#show"
+  post "checkout" => "checkouts#create"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
